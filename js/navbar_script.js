746cd6bfd43812953b38c3af9c61fb4dec7f2ab2
Stuti Patel <stutip823@gmail.com>
2023-08-31 16:04:29 -0400

    In the process of making the website responisive (part 1)

--- js/navbar_script.js
+++ js/navbar_script.js
@@ -1,21 +1,24 @@
-
 const openMenuBtn = document.getElementById("open-menu-btn");
 const closeMenuBtn = document.getElementById("close-menu-btn");
 const navExtended = document.querySelector(".extended-nav");
-
+const body = document.body;
 openMenuBtn.addEventListener('click', () => {
   navExtended.style.display = "block";
   openMenuBtn.style.display = "none";
   closeMenuBtn.style.display = "block";
+  body.style.overflow = "hidden";  // Hide scrollbars
+  body.style.position = "fixed";   // Prevent scrolling
 });
 
 closeMenuBtn.addEventListener('click', () => {
   navExtended.style.display = "none";
   closeMenuBtn.style.display = "none";
   openMenuBtn.style.display = "block";
+  body.style.overflow = "auto";    // Restore scrollbars
+  body.style.position = "static";  // Restore scrolling
 });
 
-// const navLinks = navExtended.querySelectorAll("a");
+// const navLinks = navExtended.querySelector("active");
 // navLinks.forEach(link => {
 //   link.addEventListener('click', () => {
 //     navExtended.style.display = "none";
